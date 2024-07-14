f7ffcf3ae4b2d4cd87b640d39fb0f026461e16eb
Pop2323 <omar4500455@gmail.com>
2024-07-14 20:28:14 +0300

    Are they the same

--- Two Sum.py
+++ Two Sum.py
@@ -1,10 +1,9 @@
 def two_sum(numbers, target):
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == target:
-                return ( i, j ) or (j, i)
-
-
+# 1st solution
+    # for i in range(len(numbers)):
+    #     for j in range(i + 1, len(numbers)):
+    #         if numbers[i] + numbers[j] == target:
+    #             return ( i, j ) or (j, i)
 
     #   numbers            target   valid results
 print(two_sum([1 ,2, 3],            4))    #  ((0,2), (2,0)
