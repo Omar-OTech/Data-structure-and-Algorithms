f62d82a8cf5c533915b6ee6ede40ddaeeac061b6
Pop2323 <omar4500455@gmail.com>
2024-08-12 21:51:07 +0300

    Grasshopper - Combine strings

---
+++ Grasshopper - Combine strings.py
@@ -0,0 +1,7 @@
+def combine_names(firstName, lastName):
+    return f"{firstName} {lastName}"
+
+
+print(combine_names("James", "Stevens"))     # "James Stevens"
+print(combine_names("Davy", "Back"))         # "Davy Back"
+print(combine_names("Arthur", "Dent"))       # "Arthur Dent"
