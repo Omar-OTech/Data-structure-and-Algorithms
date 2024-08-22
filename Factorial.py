5b15eee58e0b62b0a96b25dc6bc938f9e8123fb0
Pop2323 <omar4500455@gmail.com>
2024-08-22 06:42:52 +0300

    Factorial

--- Factorial.py
+++ Factorial.py
@@ -1,7 +1,13 @@
 def factorial(n):
-    return 1 if n == 0 else n * factorial(n - 1)
-
-
-def main():
-    print(factorial(5))
+    if n > 12 or n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+print(factorial(0))       # 1, "factorial for 0 is 1
+print(factorial(1))       # 1, "factorial for 1 is 1
+print(factorial(2))       # 2, "factorial for 2 is 2
+print(factorial(3))       # 6, "factorial for 3 is 6
     
