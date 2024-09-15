d7751ed8e3559f39d3e8ce72e757214984e391d8
Pop2323 <omar4500455@gmail.com>
2024-09-15 09:53:40 +0300

    Reverse String

---
+++ Javascript/ReverseString.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Test Case
+console.log(reverseString('string'));
+console.log(reverseString('javascript'));
+console.log(reverseString('Data structure'));
+console.log(reverseString('computer_science'));
