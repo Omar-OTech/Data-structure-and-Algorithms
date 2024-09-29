8997a53b86bcf108995a964932d9303fc8f2da17
Pop2323 <omar4500455@gmail.com>
2024-09-29 17:15:03 +0300

    Finding The Average

---
+++ Finding The Average.py
@@ -0,0 +1,4 @@
+d = [53, 28, 54, 84, 65, 60, 22, 93, 62, 27, 16, 25, 74, 42, 4, 42, 15, 96, 11, 70, 83, 97, 75]
+average = sum(d) / len(d)
+rounded_average = round(average, 2)
+print("The Average:", rounded_average)
