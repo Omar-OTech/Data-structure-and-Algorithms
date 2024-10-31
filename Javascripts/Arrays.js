68820c7de87454249c6818ece314edff671d05bd
Pop2323 <omar4500455@gmail.com>
2024-10-31 14:50:52 +0300

    Arrays

---
+++ Javascripts/Arrays.js
@@ -0,0 +1,16 @@
+// An array is a set of variables
+
+let favoriteFruits = ["apple", 'orange', 'strawberry'];
+let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+let randomVariable = [2, "any type works", undefined, null, true, 2.51];
+
+// Accessing an array
+myArray = ["zero", "one", "two"];
+// window.alert(myArray[0]);
+console.log(myArray[0]);
+
+myArray = ["john Doe", "Billy"];
+elementNumber = 1;
+
+// window.alert(myArray[elementNumber]);
+console.log(myArray[elementNumber]);
