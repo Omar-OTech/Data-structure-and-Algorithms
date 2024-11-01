03e093c11e0484116ce20032fa09e455709a6bb0
Pop2323 <omar4500455@gmail.com>
2024-11-01 16:02:18 +0200

    Types_of_Variables

---
+++ Javascripts/Types_of_Variables.js
@@ -0,0 +1,13 @@
+let myInteger = 12;  // 32-bit number (from -2,147,483,648 to 2,147,483,647)
+let myLong = 9310141419482; // 64-bit number (from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+let myFloat = 5.5; // 32-bit floating-point number (decimal)
+let myDouble = 9310141419482.22; // 64-bit floating-point number
+
+let myBoolean = true; // 1-bit true/false (0 or 1)
+let myBoolean2 = false;
+
+let myNotANumber = NaN;
+let NaN_Example = 0/0; // NaN Division by zero is not possiable
+
+let notDefined; // undefiend we didn't define it to anything yet
+window.alert(aRandomVariable); // undefiend 
