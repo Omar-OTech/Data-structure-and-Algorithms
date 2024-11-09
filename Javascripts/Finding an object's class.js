54e5160f0cff3d1ceccbf1118b4acd2b74a7d80a
Pop2323 <omar4500455@gmail.com>
2024-11-09 21:33:20 +0200

    Finding an object's class v2

--- Javascripts/Finding an object's class.js
+++ Javascripts/Finding an object's class.js
@@ -18,4 +18,11 @@ console.log(sum(1, 2, 3))
 console.log(sum(1, 2, 3, 4, 5))
 console.log(sum([1, 2, 3]))
 console.log(sum(4))
+// The primitive values are not considered instances of any class
+console.log(2 instanceof Number);
+console.log('xyz' instanceof String);
+console.log(true instanceof Boolean);
+console.log(Symbol() instanceof Symbol);
 
+console.log([] instanceof Object, [] instanceof Array);
+console.log([].constructor === Object, [].constructor === Array);
