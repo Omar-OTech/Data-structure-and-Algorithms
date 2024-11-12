35aca0ae72d46fa1824810c320db47d74caa0852
Pop2323 <omar4500455@gmail.com>
2024-11-12 19:30:05 +0200

    Comparing_String

---
+++ Javascript_Data-structure-and-Algorithms/Comparing_Strings.js
@@ -0,0 +1,21 @@
+let a = "hello";
+let b = "world";
+// To compare strings alphabetically, use the localeCompare() method.
+// This return native value if the reference string is alphabetically before the compare string.
+// return positive value if the reference string is alphabetically after the compare string.
+console.log(a.localeCompare(b)); // -1
+
+// use > and < operator to compare strings
+const compareStr = (a, b) => {
+    if ( a === b ) {
+        return 0;
+    }
+    if ( a > b) {
+        return 1;
+    }
+    return -1;
+}
+
+console.log(compareStr("hello", "world")); // -1
+console.log(compareStr("hello", "hello")); // 1
+console.log(compareStr("world", "hello")); // 1
