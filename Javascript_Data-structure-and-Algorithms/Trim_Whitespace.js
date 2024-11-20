c9ef302e075bba0986f598b4e477e0d20c0cc4e6
Pop2323 <omar4500455@gmail.com>
2024-11-20 23:44:45 +0200

    Trim_Whitespace

---
+++ Javascript_Data-structure-and-Algorithms/Trim_Whitespace.js
@@ -0,0 +1,12 @@
+const trim1 = "     some whitespace string       ".trim();
+const trim2 = "    this is my    ".trimStart();
+const trim3 = "    this is my    ".trimEnd(); 
+
+// const trim4 = "    this is my    ".trimLeft(); 
+// const trim5 = "    this is my    ".trimRight(); 
+
+console.log(trim1);
+console.log(trim2);
+console.log(trim3);
+// console.log(trim4);
+// console.log(trim5);
