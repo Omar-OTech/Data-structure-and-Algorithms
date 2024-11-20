1df1ade3f00ba20b0abe43bab2894f9da913f91c
Pop2323 <omar4500455@gmail.com>
2024-11-20 23:33:54 +0200

    Access_Character_at_Index_in_str

---
+++ Javascript_Data-structure-and-Algorithms/Access_Character_at_Index_in_str.js
@@ -0,0 +1,11 @@
+// charAt() -> to get character at the specified index in the string
+const str = "Hello, World!";
+console.log(str.charAt(4));
+
+// Because strings can be treadted like arrays, use the index via bracket notation
+const string = "Hello, World!";
+console.log(string[0]);
+
+// To get the character code of the character at a specified index, use charCodeAt()
+const string1 = 'Hello, World!';
+console.log(string1.charCodeAt(4));
