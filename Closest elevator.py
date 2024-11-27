b71cc4a7dc993417352ec9e4c94a993b12fb58ea
Pop2323 <omar4500455@gmail.com>
2024-11-27 20:03:24 +0200

    Closest elevator

---
+++ Closest elevator.py
@@ -0,0 +1,8 @@
+def elevator(left, right, call):
+    return 'left' if abs(left - call) < abs(right - call) else 'right'
+
+print(elevator(0, 1, 0))    # "left"
+print(elevator(0, 1, 1))    # "right"
+print(elevator(0, 1, 2))    # "right"
+print(elevator(0, 0, 0))    # "right"
+print(elevator(0, 2, 1))    # "right"
